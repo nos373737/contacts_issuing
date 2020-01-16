1d326fce2eab18df21532e626bcde5b6e8d18b9c
Nos <andrii.nos@uacv-aeu.prettl.pgn>
2020-01-16 18:12:09 +0200

    SapNumber search

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -9,7 +10,22 @@ class User(db.Model):
         return "({0}, {1}, {2})".format(self.id, self.username, self.email)
 
 
-class NumberContacts(db.Model):
+class SapNumber(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(8), unique=True, nullable=False)
+    sap_number = db.Column(db.String(8), unique=True, nullable=False)
+    contacts = db.relationship("ContactNumber", backref="sap_num")
+   
+
+class ContactNumber(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(8), nullable=False)
+    sap_num_id = db.Column(db.Integer, db.ForeignKey("sap_number.id"))
+
+    def __repr__(self):
+        return "({0})".format(self.description)
+
+
+
+
+
     
--- app/views.py
+++ app/views.py
@@ -1,21 +1,46 @@
 import pyodbc
 from app import app
-from flask import render_template, url_for
-from app.models import User, NumberContacts
+from flask import request, render_template, url_for
+from app.models import User, ContactNumber, SapNumber
+
+# @app.route('/')
+# def hello_world():
+#     conn = pyodbc.connect('Driver={SQL Server};' 'Server=UACVDB01\SQL2008EXPRESS;' 'Database=bays_contacts;' 'Trusted_Connection=no;' 'UID=snadmin;' 'PWD=sysadmin;')
+#     cursor = conn.cursor()
+#     cursor.execute('SELECT * FROM bays_contacts.dbo.test')
+#     res = []
+#     for row in cursor:
+#             res.append(row)    
+#     return str(res)
 
 @app.route('/')
-def hello_world():
-    conn = pyodbc.connect('Driver={SQL Server};' 'Server=UACVDB01\SQL2008EXPRESS;' 'Database=bays_contacts;' 'Trusted_Connection=no;' 'UID=snadmin;' 'PWD=sysadmin;')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM bays_contacts.dbo.test')
-    res = []
-    for row in cursor:
-            res.append(row)    
-    return str(res)
+def homepage():
+        return render_template('base.html')
+
+# @app.route('/check-contacts-for-sap-number')
+# def check_contacts():
+#         return render_template('check.html')
+
+@app.route('/check-contacts-for-sap-number', methods=('GET', 'POST'))
+def check_contacts_post():
+        if request.method == 'POST':
+                text = request.form['text']
+                search_result = SapNumber.query.filter_by(sap_number = text).first()
+                return render_template('check.html', search_result=search_result)
+        return render_template('check.html')
 
 @app.route('/user-list')
 def db_config():
         users = User.query.all()
-        return render_template('index.html', name = 'PRETTL AEU', users = users)
+        return render_template('users.html', name = 'PRETTL AEU', users = users)
         #return str([(u.id, u.username, u.email) for u in User.query.all()])
         
+@app.route('/numbers')
+def numbers_list():
+        numbers = ContactNumber.query.all()
+        return render_template('contacts_list.html', numbers = numbers)
+
+@app.route('/sap-numbers')
+def sap_numbers_list():
+        numbers = SapNumber.query.all()
+        return render_template('sap_number_list.html', numbers = numbers)
